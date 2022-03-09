35a00f67def377f27b375f97b6328c87675de371
Alex Starche <alex.starche@ni.com>
2022-03-09 17:17:17 -0600

    Add support for custom service initialization method

--- source/custom/nidaqmx_service.custom.cpp
+++ source/custom/nidaqmx_service.custom.cpp
@@ -2,6 +2,11 @@
 #include <nidaqmx/nidaqmx_service.h>
 
 namespace nidaqmx_grpc {
+void NiDAQmxService::initialize()
+{
+  // ni::MonikerServiceImpl::RegisterMonikerEndpoint("MonikerWriteAnalogF64Stream", MonikerWriteAnalogF64Stream);
+  // ni::MonikerServiceImpl::RegisterMonikerEndpoint("MonikerReadAnalogF64Stream", MonikerReadAnalogF64Stream);
+}
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
 ::grpc::Status NiDAQmxService::ReadAnalogF64Custom(::grpc::ServerContext* context, const ReadAnalogF64CustomRequest* request, ReadAnalogF64CustomResponse* response)
@@ -32,7 +37,7 @@ namespace nidaqmx_grpc {
 
     // Register stream instead.
     // auto status = library_->ReadAnalogF64(task, num_samps_per_chan, timeout, fill_mode);
-    response->set_status(status);
+    // response->set_status(status);
     return ::grpc::Status::OK;
   }
   catch (nidevice_grpc::LibraryLoadException& ex) {
