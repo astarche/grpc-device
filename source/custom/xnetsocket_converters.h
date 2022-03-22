ebe2c9178b49121e3c0d9a598bb5c0b0089df4a6
Alex Starche <alex.starche@ni.com>
2022-03-22 17:17:52 -0500

    Arbitrary change to converters

--- source/custom/xnetsocket_converters.h
+++ source/custom/xnetsocket_converters.h
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include <cstring>
+#include <iostream>
 #include <memory>
 
 // Helper classes and overrides of standard conversion routines go in nixnetsocket_grpc.
@@ -275,6 +276,7 @@ inline TimeValInputConverter convert_from_grpc(const pb_::Duration& input)
 
 inline void convert_to_grpc(VirtualInterfaceOutputConverter& storage, pb_::RepeatedPtrField<VirtualInterface>* output)
 {
+  std::cout << "converting something" << std::endl;
   storage.to_grpc(*output);
 }
 
